f17eab8fbab1adaa34dbf363bccde0e2ad57aa56
nadirrehman623-debug <nadirrehman623@gmail.com>
2026-06-01 13:39:24 +0000

    Mon, Jun 1, 2026, 7:09 PM +05:30

--- app.py
+++ app.py
@@ -152,8 +152,14 @@ def logout():
 def setup():
     """ Setup user's account when login for the first time """
 
-    # Make sure this is the user's first time logging in by checking if they have already set up their account
-    return None
+    # Make sure this is the user's first time logging in by checking subjects table with "user_id"
+    rows = db.execute("SELECT subject FROM subjects WHERE user_id = ?", session["user_id"])
+    # if there's subjects associated with the user's account
+    if rows:
+        return redirect("/dashboard")
+    # if the query result is empty render the setup page with the list of valid subjects for the user to choose from
+    else:
+        return render_template("setup.html", subjects=subjects)
 
 
 @app.route("/dashboard")
