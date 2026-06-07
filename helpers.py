5d04e261236285e5648a141be15515bc8928f765
nadirrehman623-debug <nadirrehman623@gmail.com>
2026-06-07 14:39:54 +0000

    Sun, Jun 7, 2026, 8:09 PM +05:30

--- helpers.py
+++ helpers.py
@@ -82,4 +82,4 @@ def model_call(system_prompt,  user_prompt, return_type="string", history = [{"r
                     ]
                 )
 
-        return response
+        return response.choices[0].message.content
