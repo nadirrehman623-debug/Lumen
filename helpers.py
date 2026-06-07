478f9e4b876f3380f74f5d6d07ace5ad8012acf7
nadirrehman623-debug <nadirrehman623@gmail.com>
2026-06-07 14:29:07 +0000

    Sun, Jun 7, 2026, 7:59 PM +05:30

--- helpers.py
+++ helpers.py
@@ -43,7 +43,7 @@ def logout_required(f):
     return decorated_function
 
 
-def model_call(system_prompt,  user_prompt, return_type="string", api_model="openai/gpt-oss-120b", history = [{"role": "none", "content": "none"}]):
+def model_call(system_prompt,  user_prompt, return_type="string", history = [{"role": "none", "content": "none"}], api_model="openai/gpt-oss-120b"):
     """
     Make the call to the LLM.
     default model = openai/gpt-oss-120b
